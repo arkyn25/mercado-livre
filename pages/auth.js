ea643d9d74ba90b1835cdebfdb6abddab787c492
Arlen Freitas <arlenfreitas1@gmail.com>
2022-04-10 19:33:49 -0300

    test

---
+++ pages/auth.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Integracao Mercado Livre</h1>
+      <a href="https://auth.mercadolivre.com.br/authorization?response_type=code&client_id=3214328881077799&redirect_uri=https://localhost:3000/auth/">
+        Autenticar com Mercado Livre
+      </a>
+
+    </div>
+  )
+}
